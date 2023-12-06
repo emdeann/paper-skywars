5a51968d8975ed1f2e5587f5995c804f8971a525
Dean <dtm44325@gmail.com>
2023-12-06 01:40:58 -0800

    Eggwars skeleton

---
+++ src/main/java/dev/deann/Managers/EggWarsGameManager.java
@@ -0,0 +1,11 @@
+package dev.deann.Managers;
+
+import dev.deann.Enum.GameType;
+import dev.deann.MinigameServer;
+
+public class EggWarsGameManager extends GameManager {
+
+    public EggWarsGameManager(MinigameServer plugin, GameType gameType) {
+        super(plugin, gameType);
+    }
+}
